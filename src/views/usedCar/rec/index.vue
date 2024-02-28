ff69c825867beafe3fae88f496358875bd94435e
hyk <1483805662@qq.com>
2024-02-28 15:01:32 +0800

    二手车推荐页优化

--- src/views/usedCar/rec/index.vue
+++ src/views/usedCar/rec/index.vue
@@ -3,6 +3,39 @@
   <SearchBar placeholder="请输入车辆名称" @handleSearch="toSearchCar" />
   <div class="main" v-loading="loading">
     <div class="rec-container min-width">
+      <div class="min-header">
+        <div class="buy-car">
+          <div class="title">我要买车</div>
+          <ul class="list price">
+            <li class="item" v-for="(item, index) in prices" :key="index">
+              {{ item }}
+            </li>
+          </ul>
+          <ul class="list type">
+            <li class="item" v-for="(item, index) in types" :key="index">
+              {{ item }}
+            </li>
+          </ul>
+          <ul class="list brand">
+            <li class="item" v-for="(item, index) in brands" :key="index">
+              {{ item }}
+            </li>
+            <li class="item all-brand">全部品牌</li>
+          </ul>
+        </div>
+        <div class="sell-car">
+          <div class="title">我要卖车</div>
+          <div class="input-button">
+            <el-input class="input" placeholder="请输入您的手机号"></el-input>
+            <el-button class="button" type="primary">卖车</el-button>
+          </div>
+          <div class="icons">
+            <div class="item balance"></div>
+            <div class="item rocket"></div>
+            <div class="item safety"></div>
+          </div>
+        </div>
+      </div>
       <div class="banner">
         <el-carousel :interval="3000" height="427px" indicator-position="none">
           <el-carousel-item v-for="item in coreRecData" :key="item.id">
@@ -64,6 +97,7 @@ import { coreRec } from "@/api";
 import { useRouter } from "vue-router";
 import useRouterStore from "@/store/module/router";
 import { useJump } from "@/hooks/jump";
+import { prices, types, brands } from "./data";
 
 const jumpDetail = useJump();
 const loading = ref(false);
@@ -133,6 +167,119 @@ const toSearchCar = (value: string) => {
   margin-top: 13px;
   flex-direction: column;
   background: #fff;
+}
+.min-header {
+  display: flex;
+  margin-top: 40px;
+  padding: 41px 30px 10px 41px;
+  background: #ffffff;
+  box-shadow: 0px 5px 22px 0px rgba(204, 204, 204, 0.25);
+  border-radius: 8px;
+  .buy-car {
+    flex: 1;
+  }
+  .sell-car {
+    width: 378px;
+    margin-left: 42px;
+    .input-button {
+      display: flex;
+      margin-bottom: 29px;
+      .input {
+        :deep(.el-input__wrapper) {
+          width: 253px;
+          height: 59px;
+          margin-right: 11px;
+          background: #f3f3f6;
+          border-radius: 4px;
+        }
+      }
+      .button {
+        width: 114px;
+        height: 59px;
+        background: #e20755;
+        border-radius: 4px;
+      }
+    }
+    .icons {
+      display: flex;
+      padding-left: 37px;
+      .item {
+        position: relative;
+        width: 36px;
+        height: 37px;
+        margin-right: 97px;
+        background-repeat: no-repeat;
+        background-size: cover;
+        &:last-of-type {
+          margin-right: 0;
+        }
+        &::after {
+          position: absolute;
+          top: 40px;
+          right: 4px;
+          font-size: 14px;
+          color: #666666;
+        }
+        &.balance {
+          background-image: url("@/assets/images/balance.png");
+          background-position: 0 -3px;
+          &::after {
+            content: "透明";
+          }
+        }
+        &.rocket {
+          background-image: url("@/assets/images/rocket.png");
+          &::after {
+            content: "高效";
+          }
+        }
+        &.safety {
+          background-image: url("@/assets/images/safety.png");
+          background-position: 0 -4px;
+          &::after {
+            content: "省心";
+          }
+        }
+      }
+    }
+  }
+  .title {
+    display: flex;
+    width: 88px;
+    margin-bottom: 20px;
+    font-weight: 600;
+    font-size: 22px;
+    color: #363636;
+    line-height: 26px;
+    text-align: left;
+  }
+  .list {
+    display: flex;
+    margin-bottom: 36px;
+    .item {
+      width: 85px;
+      font-weight: 400;
+      font-size: 14px;
+      color: #000000;
+      line-height: 16px;
+      text-align: left;
+      cursor: pointer;
+      &.all-brand {
+        display: flex;
+        align-items: center;
+        color: #bfbfbf;
+        &::after {
+          display: flex;
+          content: "";
+          width: 4px;
+          height: 8px;
+          margin-left: 4px;
+          background: url("@/assets/images/right.png") no-repeat center;
+          background-size: cover;
+        }
+      }
+    }
+  }
 }
 .banner {
   margin-top: 80px;
